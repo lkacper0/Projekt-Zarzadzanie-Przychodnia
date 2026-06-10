7095499acd697f45301f5cdcfa1fc1b28691a361
kacper <kacper.popowicz@wp.pl>
2026-06-11 00:43:23 +0200

    Fundamenty aplikacji

---
+++ resources/views/about.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <title>Kontakt</title>
+</head>
+<body>
+    <h1>Skontaktuj się z nami!</h1>
+    <p>To jest nasza nowa podstrona kontaktowa.</p>
+</body>
+</html>
