747e2b67a2e6a797e6d7a57896d1898ecf52e071
kacper <kacper.popowicz@wp.pl>
2026-06-23 13:06:43 +0200

    poprawa review

--- resources/views/admin/reviews.blade.php
+++ resources/views/admin/reviews.blade.php
@@ -22,57 +22,40 @@
 
     @if(session('success'))
         <div class="alert-success">
-
             {{ session('success') }}
-
         </div>
     @endif
 
     <div class="container">
-        <div class="container">
-            <div class="row">
-                <div class="col-1" style="border: 1px solid gray; background-color: #003366; color: white">Id</div>
-                <div class="col-2" style="border: 1px solid gray; background-color: #003366; color: white">Pacjent</div>
-                <div class="col-2" style="border: 1px solid gray; background-color: #003366; color: white">Lekarz</div>
-                <div class="col-1" style="border: 1px solid gray; background-color: #003366; color: white">Ocena</div>
-                <div class="col-4" style="border: 1px solid gray; background-color: #003366; color: white">Komentarz</div>
-                <div class="col-2" style="border: 1px solid gray; background-color: #003366; color: white">Akcje</div>
-            </div>
+
+        <div class="row">
+            <div class="col-1" style="border: 1px solid gray; background-color: #003366; color: white">Id</div>
+            <div class="col-2" style="border: 1px solid gray; background-color: #003366; color: white">Pacjent</div>
+            <div class="col-2" style="border: 1px solid gray; background-color: #003366; color: white">Lekarz</div>
+            <div class="col-1" style="border: 1px solid gray; background-color: #003366; color: white">Ocena</div>
+            <div class="col-4" style="border: 1px solid gray; background-color: #003366; color: white">Komentarz</div>
+            <div class="col-2" style="border: 1px solid gray; background-color: #003366; color: white">Akcje</div>
         </div>
 
-        <div class="container">
-            @forelse($reviews as $review)
-
+        @forelse($reviews as $review)
             <div class="row" style="border: 1px solid gray;">
-
                 <div class="col-1">{{ $review->id }}</div>
                 <div class="col-2">
-
                     @if($review->patient)
-
                         {{ $review->patient->first_name }} {{ $review->patient->last_name }}
-
                     @else
-
                         <span class="text-muted-italic">Brak danych</span>
-
                     @endif
-
                 </div>
                 <div class="col-2">
                     @if($review->doctor && $review->doctor->user)
-
                         {{ $review->doctor->user->first_name }} {{ $review->doctor->user->last_name }}
                     @else
-
                         <span class="text-muted-italic">Brak danych</span>
-
                     @endif
                 </div>
                 <div class="col-1">
-
                     {{ $review->rating }} / 5
-
                 </div>
                 <div class="col-4">{{ $review->comment ?? '-' }}</div>
 
@@ -81,17 +64,15 @@
                         @csrf
                         @method('DELETE')
 
-                        <button type="button" class="btn btn-danger">Usuń</button>
-
+                        <button type="submit" class="btn btn-danger">Usuń</button>
                     </form>
                 </div>
             </div>
-            @empty
+        @empty
             <div class="row">
-                <div class="col-2" style="padding: 20px;"> Brak opini w bazie danych. </div>
+                <div class="col-12" style="padding: 20px;"> Brak opinii w bazie danych. </div>
             </div>
-            @endforelse
-        </div>
-</div>
+        @endforelse
 
-@endsection
+    </div> </div>
+     @endsection
